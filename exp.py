04ea67a64bf2fcc5cc673df653b8d332c28c7be9
Jongcoding <ialleejy@gmail.com>
2025-06-03 02:50:23 +0900

    hello

--- exp.py
+++ exp.py
@@ -1,7 +1,7 @@
 import requests
 
 resp = requests.post(
-    "http://127.0.0.1:8000/api/analysis-requests/",
+    "https://reagan.mjsec.kr/api/analysis-requests/",
     json={"site_url": "http://kimsignals.shop"}
 )
 print(resp.status_code)   # 201
