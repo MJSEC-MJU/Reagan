a8a5e4b62a5d1d00f8f60064f4d1b34802b43768
yunttai <yoont1016@gmail.com>
2025-06-08 19:31:55 +0900

    feat: input_packet비교분석 추가

--- exp.py
+++ exp.py
@@ -2,7 +2,7 @@ import requests
 
 resp = requests.post(
     "http://127.0.0.1:8000/api/analysis-requests/",
-    json={"site_url": "http://kimsignals.shop"}
+    json={"site_url": "https://www.naver.com/"}
 )
 print(resp.status_code)   # 201
 print(resp.json())        # 위 예시와 유사한 JSON 출력
